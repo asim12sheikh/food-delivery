09c7b1ce5c5809968a37f85a7aa50d412e2715a8
sheikh wasim <wa3099286@gmail.com>
2026-09-14 08:02:16 +0530

    Fixed spelling mistake

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -72,7 +72,7 @@ const Add = ({url}) => {
                 <select onChange={onChangeHandler} value={data.category} name="category" >
                     <option value="Salad">Salad</option>
                     <option value="Rolls">Rolls</option>
-                    <option value="Deserts">Deserts</option>
+                    <option value="Desserts">Deserts</option>
                     <option value="Sandwich">Sandwich</option>
                     <option value="Cake">Cake</option>
                     <option value="Pure Veg">Pure Veg</option>
